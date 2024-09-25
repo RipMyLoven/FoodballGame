4ead0f62c3d8a54a0d8ea46f61075b6bbdf2d31f
opilane <opilane@HP-CZC2349HT0>
2024-09-25 10:43:19 +0300

    Add project files.

---
+++ FoodballGame/Player.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodballGame
+{
+    public class Player
+    {
+        public string Name { get; }
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        private double _vx, _vy;//передвижение по x y 
+        public Team? Team { get; set; } = null; //
+
+        private const double MaxSpeed = 5; //Максимальная скороть
+        private const double MaxKickSpeed = 25;
+        private const double BallKickDistance = 10;
+
+        private Random _random = new Random(); //Вызываем Рандом
+
+        //Конструктор принимет имя игрока
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        //Конструктор принимет имя,команду и x,y
+        public Player(string name, double x, double y, Team team)
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            Team = team;
+        }
+
+        //Метод устоновки позицый
+        public void SetPosition(double x, double y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+        //Устанавливатся позитсия относительно команды
+        public (double, double) GetAbsolutePosition()
+        {
+            return Team!.Game.GetPositionForTeam(Team, X, Y);
+        }
+
+        //Получение дистанцыи команды 
+        public double GetDistanceToBall()
+        {
+            var ballPosition = Team!.GetBallPosition();
+            var dx = ballPosition.Item1 - X;
+            var dy = ballPosition.Item2 - Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        //Перемешения игрока к мечу
+        public void MoveTowardsBall()
+        {
+            var ballPosition = Team!.GetBallPosition();
+            var dx = ballPosition.Item1 - X;
+            var dy = ballPosition.Item2 - Y;
+            var ratio = Math.Sqrt(dx * dx + dy * dy) / MaxSpeed;
+            _vx = dx / ratio;
+            _vy = dy / ratio;
+        }
+        //передвижения
+        public void Move()
+        {
+            if (Team.GetClosestPlayerToBall() != this)
+            {
+                _vx = 0;
+                _vy = 0;
+            }
+
+            if (GetDistanceToBall() < BallKickDistance)
+            {
+                Team.SetBallSpeed(
+                    MaxKickSpeed * _random.NextDouble(),
+                    MaxKickSpeed * (_random.NextDouble() - 0.5)
+                    );
+            }
+
+            var newX = X + _vx;
+            var newY = Y + _vy;
+            var newAbsolutePosition = Team.Game.GetPositionForTeam(Team, newX, newY);
+            if (Team.Game.Stadium.IsIn(newAbsolutePosition.Item1, newAbsolutePosition.Item2))
+            {
+                X = newX;
+                Y = newY;
+            }
+            else
+            {
+                _vx = _vy = 0;
+            }
+        }
+    }
+}
